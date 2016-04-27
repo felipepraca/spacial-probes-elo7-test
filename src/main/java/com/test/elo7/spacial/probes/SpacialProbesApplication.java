67fc91d6be3682e6acaec63b939541562af10d86
Felipe Praça <felipe.praca@buscapecompany.com>
2016-04-27 00:37:35 -0300

    Estrutura basica do projeto

---
+++ src/main/java/com/test/elo7/spacial/probes/SpacialProbesApplication.java
@@ -0,0 +1,13 @@
+package com.test.elo7.spacial.probes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacialProbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacialProbesApplication.class, args);
+	}
+
+}
